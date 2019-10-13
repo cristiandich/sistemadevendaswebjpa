caa5a9b4ce144455ab74f235b03780fb2713e6d5
Cristian Fernandes <cristiandich@gmail.com>
2019-10-13 14:48:10 -0300

    Testando a primeira classe de domiio - Categoria

--- systemvw/src/main/java/com/infocristian/systemvw/resources/CategoriaResource.java
+++ systemvw/src/main/java/com/infocristian/systemvw/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.infocristian.systemvw.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.infocristian.systemvw.domain.Categoria;
 
 @RestController
 @RequestMapping (value = "/categorias")
@@ -10,9 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
 	
-	return "Rest está funcionando";
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+
+	return lista;
 
 }
 }
