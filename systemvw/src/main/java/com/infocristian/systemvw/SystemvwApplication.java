90ca7255573aecd0c795107af8b190381dbba5e6
Cristian Fernandes <cristiandich@gmail.com>
2019-10-24 11:32:28 -0300

    Criando operações de instancia

--- systemvw/src/main/java/com/infocristian/systemvw/SystemvwApplication.java
+++ systemvw/src/main/java/com/infocristian/systemvw/SystemvwApplication.java
@@ -1,13 +1,35 @@
 package com.infocristian.systemvw;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.infocristian.systemvw.domain.Categoria;
+import com.infocristian.systemvw.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SystemvwApplication {
+public class SystemvwApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SystemvwApplication.class, args);
+	}
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		
+		Categoria cat2 = new Categoria (null, "Escritorio");
+		
+		//salvando
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
